8f07bc8996aba763efbdfdbd05c2aeef599c005a
John Benedict Solomon <johnbenedict.solomon@g.msuiit.edu.ph>
2019-04-02 15:52:44 +0800

    Customer Profile

--- app/main/service/customer_service.py
+++ app/main/service/customer_service.py
@@ -23,6 +23,28 @@ def save_new_customer(data):
         }
         return response_object, 409
 
+def update_customer(data, username):
+    update_customer = Customer.query.filter_by(username=username).first()
+    if not update_customer:
+        response_object = {
+            'status': 'fail',
+            'message': 'Customer not found'
+        }
+        return response_object, 409
+    else:
+        update_customer.username = data['username']
+        update_customer.firstname = data['firstname']
+        update_customer.lastname = data['lastname']
+        update_customer.gender = data['gender']
+        update_customer.contact_number = data['contact_number']
+        update_customer.password = data['password']
+        db.session.commit()
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully updated'
+        }
+        return response_object, 202
+
 def generate_token(customer):
     try:
         # generate the auth token
